480eeda53b44c69a9276e87e4fcb34c9c8c24d00
saputhebeast <sapumalwijekoon4@gmail.com>
2024-02-14 01:26:09 +0530

    Feat: order service rabbitmq

--- README.md
+++ README.md
@@ -5,10 +5,12 @@
 docker run -p 9411:9411 openzipkin/zipkin
 ```
 ## Ports
-| Service          | Port    |
-| ---------------- | ------- |
-| API Gateway      | 8765    |
-| Service Registry | 8761    |
-| Config Server    | 8888    |
-| ZipKin Server    | 9411    |
-| Item Service     | 8100    |
+| Service          | Port |
+|------------------|------|
+| API Gateway      | 8765 |
+| Service Registry | 8761 |
+| Config Server    | 8888 |
+| ZipKin Server    | 9411 |
+| Identity Service | 8000 |
+| Item Service     | 8100 |
+| Order Service    | 8200 |
---
+++ order-service/src/main/java/com/microservices/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,91 @@
+package com.microservices.orderservice.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name.order}")
+    private String orderQueueName;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key.order}")
+    private String orderRoutingKey;
+
+    @Value("${rabbitmq.queue.name.email}")
+    private String emailQueueName;
+
+    @Value("${rabbitmq.routing.key.email}")
+    private String emailRoutingKey;
+
+    /*
+     * spring bean for order queue
+     * */
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(orderQueueName);
+    }
+
+    /*
+     * spring bean for email queue
+     * */
+    @Bean
+    public Queue emailQueue() {
+        return new Queue(emailQueueName);
+    }
+
+    /*
+     * spring bean for exchange
+     * */
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    /*
+     * spring bean for binding order between exchange and queue using routing key
+     * */
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(orderQueue()).to(exchange()).with(orderRoutingKey);
+    }
+
+    /*
+     * spring bean for binding email between exchange and queue using routing key
+     * */
+    @Bean
+    public Binding emailBinding() {
+        return BindingBuilder.bind(emailQueue()).to(exchange()).with(emailRoutingKey);
+    }
+
+    /*
+     * message converter
+     * */
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /*
+     * configure RabbitTemplate
+     * */
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
